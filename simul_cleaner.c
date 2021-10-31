ee17f9179ccb06c2dda5215be435137ccb917b5e
gemmelos <gemmelos@gmail.com>
2021-10-31 18:26:31 +0100

    Fix bugs/typos; add switch based variant.

--- simul_cleaner.c
+++ simul_cleaner.c
@@ -6,14 +6,16 @@
 #include <time.h>
 #include <unistd.h>
 
-#define KEY_PRESSED 1
 #define KEY_RELEASED 0
-#define SIMUL_DELAY 50e6 // 50ms - Same as KarabinerElements' default
+#define KEY_PRESSED 1
+#define KEY_REPEATED 2
+#define SIMUL_THRESHOLD 50e6 // 50ms - Same as KarabinerElements' default
 #define err_exit(msg)                                                          \
     {                                                                          \
         perror(msg);                                                           \
         exit(EXIT_FAILURE);                                                    \
     }
+#define A_NON_SOURCE_KEY = -1
 
 // TODO
 // can we send keys without `msc_scan`?
@@ -54,10 +56,14 @@ target_up       = {.type = EV_KEY , .code = KEY_ESC     , .value = 0};
 /* #define SOURCE_TWO_CODE source_two_down.code */
 /* #define TARGET_CODE target_down.code */
 
+/* Only exactly 2 source keys are supported currently! */
+/* static const unsigned  int SOURCE_KEYS[] = {KEY_J, KEY_K}; */
+/* static const unsigned int TARGET_KEYS[] = {KEY_ESC}; */
+
 // Some constants:
 static const struct itimerspec ITS_ON = {
     .it_value.tv_sec = 0,
-    .it_value.tv_nsec = SIMUL_DELAY,
+    .it_value.tv_nsec = SIMUL_THRESHOLD,
     .it_interval.tv_sec = 0,  // no repeat
     .it_interval.tv_nsec = 0, // no repeat
 };
@@ -68,7 +74,6 @@ static const struct itimerspec ITS_OFF = {
     .it_interval.tv_sec = 0,  // no repeat
     .it_interval.tv_nsec = 0, // no repeat
 };
-
 
 ////////////////////////////////////////////////////////////////////////////////
 // INPUT EVENT UTILS
@@ -86,7 +91,7 @@ void write_key_event(const struct input_event *iep, bool syn_sleep)
     // If we write another event after `iep` we need to sleep first
     // for syncing of events to work (I think?)
     if (syn_sleep)
-        usleep(20000); // 0.2 ms
+        usleep(20000); // 0.2 ms or 200 us
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -120,7 +125,7 @@ void timer_disarm(timer_t tid)
 void timer_arm(timer_t tid)
 {
     /* its->it_value.tv_sec = 0; */
-    /* its->it_value.tv_nsec = SIMUL_DELAY; */
+    /* its->it_value.tv_nsec = SIMUL_THRESHOLD; */
     /* its->it_interval.tv_sec = 0;  // no repeat */
     /* its->it_interval.tv_nsec = 0; // no repeat */
     /* // Set timer */
@@ -154,12 +159,13 @@ void timer_set_up(timer_t *tidp, void (*handler)(union sigval))
 ////////////////////////////////////////////////////////////////////////////
 
 // Define your source and target keys:
-static const unsigned int SOURCE_KEYS[] = {KEY_J, KEY_K};
+static const unsigned int SOURCE_KEYS[2] = {KEY_J, KEY_K};
 static const unsigned int TARGET_KEYS[] = {KEY_ESC};
+static const size_t NUM_SOURCE_KEYS = sizeof(SOURCE_KEYS) / sizeof(int);
 
 void timer_handler(union sigval sv) { write_key_event(sv.sival_ptr, true); }
 
-size_t find_index(const int array[], const size_t size, const int value)
+inline size_t find_index(const int array[], const size_t size, const int value)
 {
     size_t index = 0;
     while (index < size && array[index] != value)
@@ -167,19 +173,12 @@ size_t find_index(const int array[], const size_t size, const int value)
     return (index == size ? -1 : index);
 };
 
-
 /**
  * Get 'opposite'/'inverse' index by XOR-ing with 1
  * 0 -> 1
  * 1 -> 0
  */
-inline size_t goi(size_t index) 
-{
-  return index ^ 0x1;
-}
-
-
-///////////////////////////////////////////////////////////////////////////
+inline size_t goi(size_t index) { return index ^ 0x1; }
 
 ////////////////////////////////////////////////////////////////////////////////
 // MAIN
@@ -200,33 +199,33 @@ int main(void)
     } targets_state = TGT_INIT;
 
     // Set up keys
-    const int key_codes[] = {KEY_J, KEY_K};
-    const size_t num_key_codes = sizeof(key_codes) / sizeof(int);
-    struct input_event events[num_key_codes][2];
+    /* const int SOURCE_KEYS[] = {KEY_J, KEY_K}; */
+    /* const size_t NUM_SOURCE_KEYS = sizeof(key_codes) / sizeof(int); */
+    struct input_event source_key_events[NUM_SOURCE_KEYS][2];
     int i;
-    for (i = 0; i < num_key_codes; i++)
+    for (i = 0; i < NUM_SOURCE_KEYS; i++)
     {
         // Key up/released
         const struct input_event down = {
-            .type = EV_KEY, .code = key_codes[i], .value = 1};
-        events[i][0] = down;
+            .type = EV_KEY, .code = SOURCE_KEYS[i], .value = 1};
+        source_key_events[i][0] = down;
         // Key down/pressed
         const struct input_event up = {
-            .type = EV_KEY, .code = key_codes[i], .value = 0};
-        events[i][1] = up;
+            .type = EV_KEY, .code = SOURCE_KEYS[i], .value = 0};
+        source_key_events[i][1] = up;
     }
 
     // Set up timers
-    timer_t timer_ids[num_key_codes];
+    timer_t timer_ids[NUM_SOURCE_KEYS];
     int j;
-    for (j = 0; j < num_key_codes; j++)
+    for (j = 0; j < NUM_SOURCE_KEYS; j++)
     {
         timer_t timerid;
         timer_ids[j] = timerid;
         struct sigevent sev = {
             .sigev_notify = SIGEV_THREAD,
             // void   *sival_ptr;    /* Pointer value */
-            .sigev_value = {.sival_ptr = &events[j][1]},
+            .sigev_value = {.sival_ptr = &source_key_events[j][1]},
             .sigev_notify_function = &timer_handler,
         };
         if (timer_create(CLOCK_REALTIME, &sev, &timerid) != 0)
@@ -238,61 +237,155 @@ int main(void)
 
     while (fread(&event, sizeof(event), 1, stdin) == 1)
     {
-        if (event.type == EV_KEY)
-        {
-            // find key code index in simul keys
-            size_t index_found =
-                find_index(key_codes, num_key_codes, event.code);
+        if (event.type != EV_KEY)
+            fwrite(&event, sizeof(event), 1, stdout);
 
-            if (event.value == KEY_PRESSED && index_found < 0)
+        // find key code index in simul keys
+        size_t source_key_found =
+            find_index(SOURCE_KEYS, NUM_SOURCE_KEYS, event.code);
+
+        /* switch (source_key_found) */
+        /* { */
+        /* // Event is not a source key */
+        /* case A_NON_SOURCE_KEY: */
+        /*     switch (event.value) */
+        /*     { */
+        /*     case KEY_PRESSED: */
+        /*         int i; */
+        /*         for (i = 0; i < NUM_SOURCE_KEYS; i++) */
+        /*         { */
+        /*             if (is_timer_armed(timer_ids[i])) */
+        /*             { */
+        /*                 timer_disarm(timer_ids[i]); */
+        /*                 write_key_event(&source_key_events[goi(i)][KEY_PRESSED], */
+        /*                                 true); */
+        /*             } */
+        /*         } */
+        /*         // Fall through 👇 */
+        /*     default: */
+        /*         // Write actual incoming key event */
+        /*         fwrite(&event, sizeof(event), 1, stdout); */
+        /*         break; */
+        /*     } */
+        /*     break; */
+        /* // Event is a source key */
+        /* case 0: */
+        /* case 1: */
+        /*     switch (event.value) */
+        /*     { */
+        /*     case KEY_PRESSED: */
+        /*         if (is_timer_armed(timer_ids[goi(source_key_found)])) */
+        /*         { */
+        /*             // Disarm all simul-key-timers */
+        /*             timer_disarm(timer_ids[goi(source_key_found)]); */
+        /*             write_key_event(&target_down, false); */
+        /*             targets_state = TGT_PRESSED_WRITTEN; */
+        /*         } */
+        /*         else */
+        /*             timer_arm(timer_ids[source_key_found]); */
+        /*         break; */
+        /*     case KEY_RELEASED: */
+        /*         if (targets_state == TGT_RELEASED_WRITTEN) */
+        /*             // Timer shouldn't be armed anymore so we don't check */
+        /*             targets_state = TGT_INIT; */
+        /*         else if (targets_state == TGT_PRESSED_WRITTEN) */
+        /*         { */
+        /*             write_key_event(&target_up, false); */
+        /*             targets_state = TGT_RELEASED_WRITTEN; */
+        /*         } */
+        /*         else if (is_timer_armed(timer_ids[source_key_found])) */
+        /*         { */
+        /*             timer_disarm(timer_ids[source_key_found]); */
+        /*             write_key_event( */
+        /*                 &source_key_events[source_key_found][KEY_PRESSED], */
+        /*                 true); */
+        /*             fwrite(&event, sizeof(event), 1, stdout); */
+        /*         } */
+        /*         break; */
+        /*     default: */
+        /*         break; */
+        /*     } */
+        /*     break; */
+        /* default: */
+        /*     break; */
+        /* } */
+        /* // We don't need `fwrite(&event, sizeof(event), 1, stdout)` anymore here */
+
+        if (event.value == KEY_PRESSED && source_key_found < 0)
+        {
+            int i;
+            for (i = 0; i < NUM_SOURCE_KEYS; i++)
             {
-                int i;
-                for (i = 0; i < num_key_codes; i++)
+                if (is_timer_armed(timer_ids[i]))
                 {
-                    if (is_timer_armed(timer_ids[i]))
-                    {
-                        timer_disarm(timer_ids[i]);
-                        write_key_event(&events[goi(i)][KEY_PRESSED], true);
-                    }
+                    timer_disarm(timer_ids[i]);
+                    write_key_event(&source_key_events[goi(i)][KEY_PRESSED],
+                                    true);
                 }
             }
-            else if (index_found == 0 || index_found == 1)
+        }
+        else if (source_key_found == 0 || source_key_found == 1)
+        {
+            // Swallow input event:
+            if (event.value == KEY_PRESSED)
             {
-                if (event.value == KEY_PRESSED)
+                if (is_timer_armed(timer_ids[goi(source_key_found)]))
                 {
-                    if (is_timer_armed(timer_ids[goi(i)]))
-                    {
-                        timer_disarm(timer_ids[(1 + index_found) % 2]);
-                        write_key_event(&target_down, false);
-                        targets_state = TGT_PRESSED_WRITTEN;
-                        continue;
-                    }
-                    else
-                    {
-                        timer_arm(timer_ids[index_found]);
-                        continue;
-                    }
+                    // Disarm all simul-timers
+                    timer_disarm(timer_ids[goi(source_key_found)]);
+                    write_key_event(&target_down, false);
+                    targets_state = TGT_PRESSED_WRITTEN;
+                    continue;
                 }
-                else if (event.value == KEY_RELEASED)
+                else
                 {
-                    if (targets_state == TGT_RELEASED_WRITTEN)
-                    {
-                        // timer shouldn't be armed anymore
-                        // in this state so we don't check
-                        targets_state = TGT_INIT;
-                        continue;
-                    }
-                    else if (targets_state == TGT_PRESSED_WRITTEN)
-                    {
-                        write_key_event(&target_up, false);
-                        targets_state = TGT_RELEASED_WRITTEN;
-                        continue;
-                    }
-                    else
-                    {
-                        timer_disarm(timer_ids[index_found]);
-                        write_key_event(&events[i][KEY_PRESSED], true;
-                    }
+                    timer_arm(timer_ids[source_key_found]);
+                    continue;
+                }
+            }
+            else if (event.value == KEY_RELEASED)
+            {
+                if (targets_state == TGT_RELEASED_WRITTEN)
+                {
+                    // Timer shouldn't be armed anymore
+                    // in this state so we don't check
+                    targets_state = TGT_INIT;
+                    continue;
+                }
+                else if (targets_state == TGT_PRESSED_WRITTEN)
+                {
+                    write_key_event(&target_up, false);
+                    targets_state = TGT_RELEASED_WRITTEN;
+                    continue;
+                }
+                // We don't want to do this if the timer isn't actually
+                // going anymore, i.e. when we've pressed `j` down for
+                // longer than the threshold and the timer handler has
+                // already written `j` down-event, right?
+                // Not sure why original code isn't doing this but seems
+                // to be working fine? Maybe because this case never happens
+                // I never hold down `j` for >=50ms (threshold ms) to
+                // trigger the handler?
+                else if (is_timer_armed(timer_ids[source_key_found]))
+                {
+                    // So for example an `j`-up event
+                    // Do we know `k` is down/being-swallowed rn?
+                    // - if it is then the even after this one will determine
+                    // what should happen with `k` down
+                    // - if it is not then we don't really care at all.
+                    //
+                    // So in case `j` down-event timer is running then we
+                    // know we have no release `j` key before the threshold
+                    // so we should disable the timer and before the action
+                    // the timer handler would perform, i.e. send/transmit/
+                    // write-to-stdout the `j` key-down event
+                    // Afterwards we fall through the rest of the code block
+                    // to arrive at the `fwrite` call that will write the
+                    // actual `j` up-event.
+                    timer_disarm(timer_ids[source_key_found]);
+                    write_key_event(
+                        &source_key_events[source_key_found][KEY_PRESSED],
+                        true);
                 }
             }
         }
